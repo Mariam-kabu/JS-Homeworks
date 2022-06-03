02a25ee77f2aaaa611655514ad6e3181554b1ba7
Mariam-Kabu <mariamkabulovi@gmail.com>
2022-06-03 13:15:35 +0400

    task 3

---
+++ Control-Flow-Loops/practice/task-3.js
@@ -0,0 +1,10 @@
+let val;
+if (val == 'a') 
+    console.log('a');
+else if (val == 'b' || val == 'c' || val == 'd') {
+    //code bolocks weren't given
+} 
+else if (val == 'e') 
+    console.log('others');
+else 
+    console.log('unknown');
