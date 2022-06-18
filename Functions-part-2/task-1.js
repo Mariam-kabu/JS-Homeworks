391181568a0e14b0ac0fe49b82bc5efae3faa10b
Mariam-Kabu <mariamkabulovi@gmail.com>
2022-06-18 22:33:21 +0400

    task 1

--- Functions-part-2/task-1.js
+++ Functions-part-2/task-1.js
@@ -1,11 +1,15 @@
-const mix = (...functions) => functions.reduce((ans, callback) => callback(ans), undefined);
-
-const answer = mix(() => {
+const mix = (...functions) => {
+    for (f of functions)
+        if (typeof f !== 'function') throw new TypeError ('Every param Should be a function');
+    const answer = functions.reduce((ans, callback) => callback(ans), undefined);
+    return answer;
+}
+const output = mix(() => {
     return 0;
 }, (prev) => {
-    return prev + 1;
+    return prev + 8;
 }, (prev) => {
-    return prev * 2;
-})
+    return prev * 5;
+});
 
-console.log(answer);
+console.log(output);
